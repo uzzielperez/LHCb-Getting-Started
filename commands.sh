9ffd55ae7729cfa89c4ad993a5ca68cbdfff8e09
Cilicia Uzziel Perez <uzzie.perez@cern.ch>
2024-08-02 14:07:54 +0800

    commands

--- commands.sh
+++ commands.sh
@@ -5,9 +5,12 @@ function show_commands {
     echo "Useful Commands:"
     echo "----------------"
     echo "1. Activate environment: "
-    echo "   source env/bin/activate"
+    echo "   source lhcbenv/bin/activate"
     echo "2. Increase Git buffer size to handle large pushes"
     echo " git config --global http.postBuffer 157286400"
+    echo "3. Test image-coor.py"
+    echo " cd detector" 
+    echo " python image-coor.py"
 }
 
 # Run the function
